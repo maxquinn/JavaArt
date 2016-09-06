0b95916522e0ff26797ff9a3c19323b02202205f
maxquinn <max.t.quinn@gmail.com>
2016-09-07 00:54:32 +1200

    first artwork created

--- src/MakeArt.java
+++ src/MakeArt.java
@@ -1,30 +1,47 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.Console;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+import java.util.List;
 
 public class MakeArt {
     //Fields
     private int gridSize;
-    private Set<Color> colorsSet;
-    private int[][] pixelGrid;
+    private List<Color> coloursList;
+    private Color[][] pixelGrid;
 
     //classes
     public MakeArt() {
-        gridSize = 20;
+        gridSize = 4096;
                 //4096;
-        colorsSet = fillSet();
+        coloursList = fillSet();
+        System.out.println("Colors Generated Successfully");
         makeGrid();
-        drawGrid();
+        createImage();
+        System.out.println("Done!");
     }
 
-    public Set<Color> fillSet() {
-        Set<Color> returnSet = new HashSet<>();
-        return returnSet;
+    public List<Color> fillSet() {
+        System.out.println("Preparing colours ... Please Wait!");
+        List<Color> returnList = new ArrayList<>();
+        for (int r = 0; r <= 255; r++) {
+            for (int g = 0; g <= 255; g++) {
+                for (int b = 0; b <= 255; b++) {
+                        returnList.add(new Color(r,g,b));
+                    }
+                }
+            }
+
+        return returnList;
     }
 
     public void makeGrid() {
-        int count = 1;
-        pixelGrid = new int[gridSize][gridSize];
+        System.out.println("Generating pixel grid");
+        int count = 0;
+        pixelGrid = new Color[gridSize][gridSize];
 
         for (int i = 0; i < pixelGrid.length; i++) {
             int row = 0;
@@ -32,11 +49,11 @@ public class MakeArt {
 
             while (col >= 0) {
                 if (col > row) {
-                    pixelGrid[row][col] = count;
+                    pixelGrid[row][col] = coloursList.get(count);
                     count++;
                     row++;
                 } else {
-                    pixelGrid[row][col] = count;
+                    pixelGrid[row][col] = coloursList.get(count);
                     count++;
                     col--;
                 }
@@ -51,6 +68,22 @@ public class MakeArt {
             }
             System.out.println("");
         }
+    }
+
+    public void createImage() {
+        try {
+        BufferedImage bufferedImage = new BufferedImage(pixelGrid.length, pixelGrid[0].length, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < pixelGrid.length; x++) {
+            for (int y = 0; y < pixelGrid[x].length; y++) {
+                bufferedImage.setRGB(x, y, pixelGrid[x][y].getRGB());
+            }
+        }
+
+        File outputFile = new File("art.png");
+            ImageIO.write(bufferedImage, "png", outputFile);
+        } catch (java.io.IOException e) { System.out.print("error: " + e);}
+
+
     }
 
     public static void main(String[] args) {
